5ed91dd93dee1f25ccec38436b0e9421f77a2c5e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-02 12:10:13 -0400

    Print blockchain at the end

--- blockchain.py
+++ blockchain.py
@@ -9,9 +9,9 @@ def last_value():
 def add_value(tx_amount):
     """add a single block"""
     blockchain.append([last_value(), tx_amount])
-    print(blockchain)
 
 
 add_value(1)
 add_value(2)
 add_value(3)
+print(blockchain)
