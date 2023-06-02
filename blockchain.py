cded7bc8132fe8fee655aab09ee3005727bdcfc4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-02 12:06:23 -0400

    Allow tx_amount

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,11 @@
-blockchain = [1]
+blockchain = [0]
 
 
-def add_value():
+def add_value(tx_amount):
     blockchain.append([blockchain[-1], 5.3])
     print(blockchain)
 
 
-add_value()
+add_value(1)
+add_value(2)
+add_value(3)
