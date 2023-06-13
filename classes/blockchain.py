119af5ff61cb582cafd19195313e1c41a4c047ac
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-13 15:23:25 -0400

    Add self.mining_reward

--- classes/blockchain.py
+++ classes/blockchain.py
@@ -12,6 +12,7 @@ class Blockchain:
         self.global_sender = "test_sender"
         self.global_sender_balance = 10
         self.users = {self.global_sender}
+        self.mining_reward = 5.0
 
         # read blockchain from disk
         make_storage_directories()
@@ -29,7 +30,7 @@ class Blockchain:
         """get last value of blockchain"""
         return self.blockchain[-1]
 
-    def add_block(recipient, tx_amount=1):
+    def add_block(self, recipient, tx_amount=1):
         """add a single block"""
         if float(tx_amount) > self.global_sender_balance:
             return "Not enough balance."
@@ -42,47 +43,47 @@ class Blockchain:
         self.global_sender_balance -= float(tx_amount)
         return True
 
-    def mine_block(recipient, tx_amount):
+    def mine_block(self, recipient, tx_amount):
         """ Mine a block """
-        global_open_txs_copy = global_open_txs[:]
+        global_open_txs_copy = self.global_open_txs[:]
 
         global_open_txs_copy.extend([
-            OrderedDict([("sender", global_sender), ("recipient",
+            OrderedDict([("sender", self.global_sender), ("recipient",
                         recipient), ("tx_amount", float(tx_amount)), ("type", "mine")]),
-            OrderedDict([("sender", None), ("recipient", global_sender),
-                        ("tx_amount", MINING_REWARD), ("type", "reward")])
+            OrderedDict([("sender", None), ("recipient", self.global_sender),
+                        ("tx_amount", self.mining_reward), ("type", "reward")])
 
         ])
 
         # hashing
-        # new_checkhash = str(get_last_block().values())
-        new_checkhash = generate_hash(get_last_block())
+        # new_checkhash = str(self.get_last_block().values())
+        new_checkhash = generate_hash(self.get_last_block())
 
-        global_open_txs = global_open_txs_copy
+        self.global_open_txs = global_open_txs_copy
 
         # Do POW
-        pow_num = generate_pow()
+        pow_num = self.generate_pow()
 
-        blockchain.append({
+        self.blockchain.append({
             "checkhash": new_checkhash,
-            "txs": global_open_txs,
+            "txs": self.global_open_txs,
             "pow_num": pow_num
         })
 
-        global_open_txs = []
+        self.global_open_txs = []
 
-    def generate_pow():
+    def generate_pow(self):
         """ Generate proof of work. """
-        current_chechhash = generate_hash(get_last_block())
+        current_chechhash = generate_hash(self.get_last_block())
         current_proof_num = 0
-        while not verify_proof(global_open_txs, current_chechhash, current_proof_num):
+        while not verify_proof(self.global_open_txs, current_chechhash, current_proof_num):
             current_proof_num += 1
         return current_proof_num
 
-    def verify_blockchain():
+    def verify_blockchain(self):
         """ Make sure checkhash matches actual previous block content  """
         prev_block = None
-        for block in blockchain:
+        for block in self.blockchain:
             if not prev_block:
                 prev_block = block
                 continue
@@ -94,10 +95,10 @@ class Blockchain:
                 prev_block = block
         return True
 
-    def get_balance(username):
+    def get_balance(self, username):
         """ Print balance of a single user """
         balance = 0
-        for block in blockchain:
+        for block in self.blockchain:
             for tx in block["txs"]:
                 if tx["sender"] == username or tx["recipient"] == username:
                     balance += tx["tx_amount"]
