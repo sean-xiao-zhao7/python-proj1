0ce25808345fab4c5b1a58018aa3945de017c443
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-15 13:34:19 -0400

    Move blockchain network into root dir

--- blockchain_network.py
+++ blockchain_network.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from classes.blockchain import Blockchain
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,11 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def get_ui():
     return ''
+
+
+@app.route('/blockchain', methods=['GET'])
+def get_blockchain():
+    return Blockchain()
 
 
 if __name__ == '__main__':
