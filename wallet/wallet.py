ac140dcc5dbe53612c85e8fb924d3a79004d4e0d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-15 12:50:17 -0400

    Add pr/pub key generation

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,4 +1,10 @@
+from Crypto.PublicKey import RSA
+import Crypto.Random
+import binascii
+
 
 class Wallet:
     def __init__(self):
-        pass
+        pr_key = RSA.generate(1024, Crypto.Random.new().read)
+        pub_key = pr_key.publickey()
+        return (binascii.hexlify(pr_key.exportKey(format='DER')).decode('ascii'), binascii.hexlify(pub_key.exportKey(format='DER')).decode('ascii'))
