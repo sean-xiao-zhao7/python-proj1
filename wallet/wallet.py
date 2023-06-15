477f151023ce63e62dd57899d5d2ad5e24b70a8e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-15 12:51:50 -0400

    Fix saving pub/pr keys in Wallet

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -7,4 +7,7 @@ class Wallet:
     def __init__(self):
         pr_key = RSA.generate(1024, Crypto.Random.new().read)
         pub_key = pr_key.publickey()
-        return (binascii.hexlify(pr_key.exportKey(format='DER')).decode('ascii'), binascii.hexlify(pub_key.exportKey(format='DER')).decode('ascii'))
+        self.private_key = binascii.hexlify(
+            pr_key.exportKey(format='DER')).decode('ascii')
+        self.public_key = binascii.hexlify(
+            pub_key.exportKey(format='DER')).decode('ascii')
