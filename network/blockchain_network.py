d4da214735ac098734dda51bbfc4d9158fd9eb8c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-15 13:15:43 -0400

    Add route /

--- network/blockchain_network.py
+++ network/blockchain_network.py
@@ -4,5 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
+@app.route('/', methods=['GET'])
+def get_ui():
+    return ''
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
